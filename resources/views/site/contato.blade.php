61cad82fa0f4d871fabbd1374972c056bc658517
Pluer <ramonpvlima2@hotmail.com>
2024-01-20 11:49:07 -0300

    20-01-2024

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,64 +1,37 @@
-        <div class="topo">
+@extends('site.layout.basico')
 
-            <div class="logo">
-                <img src="{{ asset('img/logo.png') }}">
-            </div>
+@section('titulo', 'Contato')
 
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.principal') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobreNos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Entre em contato conosco</h1>
+        </div>
+        <div class="informacao-pagina">
+            <div class="contato-principal">
+                @component('site.layout._components.form_contato')   
+                    <p>A nossa equipe analisará a sua mensagem  e retornaremos o mais rápido possível!</p>
+                    <p>Nosso tempo médio de resposta é de 48 horas.</p>
+                @endcomponent
             </div>
         </div>
-
-        <div class="conteudo-pagina">
-            <div class="titulo-pagina">
-                <h1>Entre em contato conosco</h1>
-            </div>
-
-            <div class="informacao-pagina">
-                <div class="contato-principal">
-                    <form>
-                        <input type="text" placeholder="Nome" class="borda-preta">
-                        <br>
-                        <input type="text" placeholder="Telefone" class="borda-preta">
-                        <br>
-                        <input type="text" placeholder="E-mail" class="borda-preta">
-                        <br>
-                        <select class="borda-preta">
-                            <option value="">Qual o motivo do contato?</option>
-                            <option value="">Dúvida</option>
-                            <option value="">Elogio</option>
-                            <option value="">Reclamação</option>
-                        </select>
-                        <br>
-                        <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                        <br>
-                        <button type="submit" class="borda-preta">ENVIAR</button>
-                    </form>
-                </div>
-            </div>  
+    </div>
+    <div class="rodape">
+        <div class="redes-sociais">
+            <h2>Redes sociais</h2>
+            <img src="{{ asset('img/facebook.png') }}">
+            <img src="{{ asset('img/linkedin.png') }}">
+            <img src="{{ asset('img/youtube.png') }}">
         </div>
-
-        <div class="rodape">
-            <div class="redes-sociais">
-                <h2>Redes sociais</h2>
-                <img src="{{ asset('img/facebook.png') }}">
-                <img src="{{ asset('img/linkedin.png') }}">
-                <img src="{{ asset('img/youtube.png') }}">
-            </div>
-            <div class="area-contato">
-                <h2>Contato</h2>
-                <span>(11) 3333-4444</span>
-                <br>
-                <span>supergestao@dominio.com.br</span>
-            </div>
-            <div class="localizacao">
-                <h2>Localização</h2>
-                <img src="{{ asset('img/mapa.png') }}">
-            </div>
+        <div class="area-contato">
+            <h2>Contato</h2>
+            <span>(11) 3333-4444</span>
+            <br>
+            <span>supergestao@dominio.com.br</span>
         </div>
-    </body>
-</html>
+        <div class="localizacao">
+            <h2>Localização</h2>
+            <img src="{{ asset('img/mapa.png') }}">
+        </div>
+    </div>
+@endsection
