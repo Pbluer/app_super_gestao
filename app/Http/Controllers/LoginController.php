caa0adfc1459bd626803159387bad1bfb4159c62
Pluer <ramonpvlima2@hotmail.com>
2024-02-19 19:43:49 -0300

    19-02-24

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
 class LoginController extends Controller
 {
-    //
+    public function index(){
+        return view('site.login', [ 'titulo' => 'Login']);
+    }
+    
+    public function autenticar( Request $request){
+
+        //regras de validaçao dos campos
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        // feedback das validacao das regras
+
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório.',
+            'senha.required' => 'O campo senha é obrigatório.'
+        ];
+
+        // se nao passar pelo validate
+        $request->validate($regras,$feedback);
+
+
+        $email = $request->get('usuario');
+        $senha = $request->get('senha');
+
+        $user = new User();
+
+        $existe = $user->where('email', $email)->where('password', $senha)->get()->first();
+        echo '<br>';
+        dd($existe);      
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,12 @@ Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobreNos');
 Route::get('/contato','ContatoController@contato')->name('site.contato');
 Route::post('/contato','ContatoController@salvar')->name('site.contato');
 
+Route::get('/login','LoginController@index')->name('site.login');    
+Route::post('/login','LoginController@autenticar')->name('site.login');    
+
 Route::prefix('/app')->group( function(){    
 
-    Route::get('/login', fn() => 'login' )
-        ->name('app.login');    
+   
 
     Route::get('/clientes', fn() => 'clientes' )
         ->name('app.clientes');
